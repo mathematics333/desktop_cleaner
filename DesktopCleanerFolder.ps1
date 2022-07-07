2484f98286c5ff565a38340ab762e6b3c333b1db
Matthew Fuller <63489507+mathematics333@users.noreply.github.com>
2022-07-07 14:01:10 -0500

    Add files via upload

---
+++ DesktopCleanerFolder.ps1
@@ -0,0 +1,63 @@
+﻿# Directory for desktop_cleaner
+
+Set-Location C:\\1
+
+mkdir Archive
+
+Set-Location Archive
+
+mkdir Documents\\TextFiles
+
+mkdir Documents\\Microsoft\\Word
+
+mkdir Documents\\PDF\
+
+mkdir Documents\\Microsoft\\Excel
+
+mkdir Documents\\Other\\System
+
+mkdir Documents\\Presentations
+
+mkdir Media\\Audio
+
+mkdir Media\\Video
+
+mkdir Media\\Images
+
+mkdir Other\\Internet
+
+mkdir Other\\Compressed
+
+mkdir Other\\Disc
+
+mkdir Other\\Executables
+
+mkdir Other\\Fonts
+
+mkdir Other\\Uncategorized
+
+mkdir Programming\\AHK
+
+mkdir Programming\\C
+
+mkdir Programming\\Database
+
+mkdir Programming\\Java
+
+mkdir Programming\\Dart
+
+mkdir Programming\\Python
+
+mkdir Programming\\Shell
+
+mkdir Programming\\Swift
+
+mkdir Programming\\html
+
+mkdir Models
+
+mkdir TEMP
+
+
+
+
